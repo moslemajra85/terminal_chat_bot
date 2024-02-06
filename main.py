60443d1ce59720812d066836c31c18a42540e456
moslemajra85 <144964665+moslemajra85@users.noreply.github.com>
2024-02-06 07:37:42 +0100

    Use ConversationBufferMemory to save conversations

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from langchain.chat_models import ChatOpenAI
-from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate,MessagesPlaceholder
 from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory
 from dotenv import load_dotenv
 
 load_dotenv()
 
 chat = ChatOpenAI()
-prompt = ChatPromptTemplate(input_variables=["content"],
-                            messages=[HumanMessagePromptTemplate.from_template("{content}")])
+
+# memory_key is the name of the additional variable added by memory
+# return_messages is set to true because we are working with a conversational LLM
+memory = ConversationBufferMemory(memory_key="messages", return_messages=True)
+prompt = ChatPromptTemplate(
+    input_variables=["content", "messages"],
+                            messages=[MessagesPlaceholder(variable_name="messages"),HumanMessagePromptTemplate.from_template("{content}")])
 
 chain = LLMChain(
     llm=chat,
-    prompt=prompt
+    prompt=prompt,
+    memory=memory
 )
 
 while True:
